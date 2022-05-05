3176224ad11efd562322775cffb61a9a5c02382c
idaniths <idan.cheaproot@iths.se>
2022-05-05 09:17:42 +0200

    added 'getRecipeByQuery'

--- server/db/recipes.ts
+++ server/db/recipes.ts
@@ -3,3 +3,9 @@ import recipeModel from "./models/recipeModel";
 export const getAllRecipes = async () => {
     return await recipeModel.find();
 }
+
+export const getRecipeByQuery = async (query: any) => {
+    return await recipeModel.find(
+        {"title" :{ '$regex' : query.search, '$options' : "i"} });
+    
+}
--- server/routes/recipe.ts
+++ server/routes/recipe.ts
@@ -1,13 +1,21 @@
 import express, {Request, Response, json}  from 'express';
-import {getAllRecipes} from '../db/recipes';
+import {getAllRecipes, getRecipeByQuery} from '../db/recipes';
 
 
 const router = express.Router();
 
 
 router.get('/', async (req: Request, res: Response) => {
-    const recipes = await getAllRecipes();
-    res.json(recipes);
+    const query = req.query;
+    if(Object.keys(query).length > 0) {
+        if(Object.keys(query).includes("search")) {
+            const recipes = await getRecipeByQuery(query);
+            res.json(recipes);
+        }
+    } else {
+        const recipes = await getAllRecipes();
+        res.json(recipes);
+}
 });
 
 
