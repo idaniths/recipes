9bfa7595621c52fcb7dbbd148a24a72f898ece76
idaniths <idan.cheaproot@iths.se>
2022-05-19 15:13:23 +0200

    changed router

--- server/server.ts
+++ server/server.ts
@@ -1,21 +1,24 @@
 import express, { Request, Response, json } from 'express';
-import mongoose from 'mongoose';
+// import mongoose from 'mongoose';
+import {connect} from 'mongoose';
 import recipeRouter from './routes/recipe';
 import categoryRouter from './routes/category';
 import cors from 'cors'
+import dotenv from 'dotenv';
 
+dotenv.config();
+
+const connectionString: string = (process.env.MONGO_DB_CONNECTION_STRING as string);
+
+connect(connectionString).then(() => {
+    console.log('Connected to MongoDB');
+})
 
 const url = "mongodb+srv://idan:lPNQkHrQLnDPSOEr@cluster0.stdjt.mongodb.net/recipesPage?retryWrites=true&w=majority"
 
 // mongoose.connect(url) 
 
-if (process.env.MONGO_DB_CONNECTION_STRING) {
-    mongoose.connect(process.env.MONGO_DB_CONNECTION_STRING).then(() => {
-        app.listen(port, () => console.log('listening to port: ' + port));
-    })
-} else {
-    console.log('Configuration MONGO_DB_CONNECTION_STRING not set')
-}
+
     // .then( () => {
     //     console.log('Connected to the database ')
     // })
@@ -27,6 +30,16 @@ const app = express();
 app.use(json());
 app.use(cors());
 const port = process.env.PORT || 3000;
+
+
+// if (process.env.MONGO_DB_CONNECTION_STRING) {
+//     mongoose.connect(process.env.MONGO_DB_CONNECTION_STRING).then(() => {
+//         app.listen(port, () => console.log('listening to port: ' + port));
+//     })
+// } else {
+//     console.log('Configuration MONGO_DB_CONNECTION_STRING not set')
+// }
+
 
 app.get('/', (req: Request, res: Response) => {     
     res.send('HOOOO');
